353faeceed9b222447a5c803ba8c26c325c8aa43
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-03-26 20:41:11 +0200

    Add association and validations to the Note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,8 +1,11 @@
 class Note < ApplicationRecord
-  validates :title, presence: true
 
   belongs_to :user
+  has_many :videos
+
+  validates :title, presence: true
 
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
